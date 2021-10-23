9ac3f0776429ed1432023551b6d5d161edd3fef3
徐世成 <1213089093@qq.com>
2021-10-23 19:41:51 +0800

    modify Instruction.md

--- myApp/src/pages/index/index.vue
+++ myApp/src/pages/index/index.vue
@@ -1,20 +1,38 @@
+<!--
+ * @Descripttion:
+ * @version:
+ * @Author: Evildoer98
+ * @Date: 2021-10-23 15:45:42
+ * @LastEditors: Evildoer98
+ * @LastEditTime: 2021-10-23 19:29:42
+-->
 <template>
   <view class="index">
-    <NumberDisplay/>
-    <NumberSubmit/>
+    <!-- <NumberDisplay/>
+    <NumberSubmit/> -->
+    <text>{{msg}}</text>
   </view>
 </template>
 
 <script>
-import NumberDisplay from '../../components/NumberDisplay.vue'
-import NumberSubmit from '../../components/NumberSubmit.vue'
+// import NumberDisplay from '../../components/NumberDisplay.vue'
+// import NumberSubmit from '../../components/NumberSubmit.vue'
 
+import {ref} from 'vue'
 export default {
-  name: 'Index',
-  components: {
-    NumberDisplay,
-    NumberSubmit
+  // name: 'Index',
+  // components: {
+  //   NumberDisplay,
+  //   NumberSubmit
+  // }
+
+  setup () {
+    const msg = ref('My Demo')
+    return {
+      msg
+    }
   }
+
 }
 </script>
 
@@ -24,7 +42,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000;
   margin-top: 60px;
 }
 </style>
